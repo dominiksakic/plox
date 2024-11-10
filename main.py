56e3eb5f5d59da96b906a082530ccc9c43304380
dominiksakic <dominik.sakic@yahoo.com>
2024-11-10 20:50:50 +0900

    feat: read a file and output the bytes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 import sys
+
 class Plox:
     @staticmethod
     def main():
@@ -6,9 +8,25 @@ class Plox:
             print("Usage: plox [script]")
             sys.exit(64)
         elif len(sys.argv) == 2:
+            Plox.run_file(sys.argv[1])
             print("Run file")
         else:
             print("REPL")
+
+    @staticmethod
+    def run_file(file_path):
+        path = Path(file_path)
+        data = path.read_bytes()
+
+
+        Plox.run(data)
+
+    @staticmethod
+    def run(source):
+        tokens = source # Prints out numbers.
+
+        for token in tokens:
+            print(token)
 
 if __name__ == "__main__":
    Plox.main()
