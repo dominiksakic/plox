f55296aac459bf28966d9dc1b73b2c6001bbf0e6
dominiksakic <dominik.sakic@yahoo.com>
2024-11-10 20:37:28 +0900

    feat: create entry point to run scripts and REPL

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Hello World!")
+import sys
+class Plox:
+    @staticmethod
+    def main():
+        if len(sys.argv) > 2:
+            print("Usage: plox [script]")
+            sys.exit(64)
+        elif len(sys.argv) == 2:
+            print("Run file")
+        else:
+            print("REPL")
 
-
-
+if __name__ == "__main__":
+   Plox.main()
