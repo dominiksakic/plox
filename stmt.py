bdfa48c31a258e02fccf30e45f065f12986bc87a
dominiksakic <dominik.sakic@yahoo.com>
2024-12-11 21:27:07 +0900

    chore: fix Stmt class to implement Visitor Pattern

--- stmt.py
+++ stmt.py
@@ -1,9 +1,29 @@
-class Stmt():
-    def __init__(self):
-        pass 
+from abc import ABC
+from typing import TypeVar, Generic, Any
 
-    def print(self, expression):
+T = TypeVar('T')
+
+class Visitor(Generic[T], ABC):
+    @abstractmethod
+    def visit_expr_stmt(self,expr) -> T:
+        pass
+        
+class Stmt(ABC):
+    @abstractmethod
+    def accept(self, visitor: Visitor[T]) -> T:
         pass
 
-    def expression(self, expression):
-        pass
+class ExprStmt(Stmt):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor: Visitor[T]) -> T:
+        return visitor.visit_expr_stmt(self)
+
+
+class PrintStmt(Stmt):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor: Visitor[T]) -> T:
+        return visitor.visit_expr_stmt(self)
